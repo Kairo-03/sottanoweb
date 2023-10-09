6a4bcba2a0327fc92a85af9c506eaa6dd1eb29e6
Nahuel <nahuelromerojobs@gmail.com>
2023-10-09 16:51:15 -0300

    Preparado para mersh

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Contenido(models.Model):
+    titulo = models.CharField(max_length=200)
+    texto = models.TextField()
+    imagen = models.ImageField(upload_to='contenido_imagenes/')
+
+    def __str__(self):
+        return self.titulo
